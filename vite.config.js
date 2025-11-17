6c416728d80d6addafe0096cd5387eeb09f02c86
P SRI HARSHITHA <2300032723@kluniversity.in>
2025-11-17 14:13:19 +0530

    updated 3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/endlab/',     // 👈 ADD THIS LINE
+  base: '/endlab/',
   plugins: [react()],
 })
